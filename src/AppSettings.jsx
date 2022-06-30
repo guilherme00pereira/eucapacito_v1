b098e4f64022c89cdfc2102dbb58b4eed7b59b35
GUILHERME PEREIRA DE SOUZA E ALVES <guilheps@trt3.jus.br>
2022-06-30 12:37:59 -0300

    v_0.4.7

--- src/AppSettings.jsx
+++ src/AppSettings.jsx
@@ -1,9 +1,14 @@
 import { Container } from '@mui/material';
 import { Outlet } from 'react-router-dom';
+import ReactGA from 'react-ga';
 
 import './App.css';
 
+const TRACKING_ID = "G-NGP1WFMVFV";
+
 const AppSettings = () => {
+  ReactGA.initialize(TRACKING_ID);
+  ReactGA.pageview(window.location.pathname + window.location.search);
   return (
     <Container className="App" sx={{ 
       p: 0,
