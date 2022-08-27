e39a0f4eee4939de8174944a75931ae542e3468e
Guilherme Pereira <guilherme00pereira@gmail.com>
2022-08-27 15:11:37 -0300

    v_0.5.22

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import TagManager from 'react-gtm-module'
 
 import 'swiper/css/bundle';
 
 import './fonts.css';
 import './index.css';
+
+const tagManagerArgs = {
+    gtmId: 'GTM-K77MBFD'
+}
+
+TagManager.initialize(tagManagerArgs)
 
 ReactDOM.render(
   <React.StrictMode>
