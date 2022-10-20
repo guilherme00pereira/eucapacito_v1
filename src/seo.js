13a7d87c61fc984da5a35a2a581cc44b479fa462
Guilherme Pereira <guilherme00pereira@gmail.com>
2022-10-20 10:09:57 -0300

    v_1.0.7

--- src/seo.js
+++ src/seo.js
@@ -5,15 +5,15 @@ const SEO = ({metadata}) => {
         <Head>
             <title>Eu Capacito {metadata.title}</title>
             {metadata.og_image &&
-                <meta property='og:image' content={metadata.og_image[0].url} />
+                <meta name="image" property='og:image' content={metadata.og_image[0].url} />
             }
+            <meta name="title" property="og:title" content={metadata.og_title} />
             <meta name="description" content={metadata.description} />
             <link rel="canonical" href={metadata.canonical} />
             <meta property="og:locale" content="pt_BR" />
             <meta property="og:url" content={metadata.og_url} />
             <meta property="og:site_name" content="Eu Capacito" />
             <meta property="og:type" content="website" />
-            <meta property="og:title" content={metadata.og_title} />
             <meta property="og:description" content={metadata.og_description} />
             <meta property="article:modified_time" content={metadata.article_modified_time} />
         </Head>
