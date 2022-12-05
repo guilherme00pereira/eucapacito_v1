4f2a4fcd4385e44f489174401ef32ccab36357e7
Guilherme Pereira <guilherme00pereira@gmail.com>
2022-12-05 11:06:56 -0300

    v_1.0.21

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
     try {
       // this should be the actual path not a rewritten path
       // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-      if(req.query.entity === 'blog')
+      if(req.query.entity === 'post')
       {
         await res.revalidate(req.query.slug)  
       } 
